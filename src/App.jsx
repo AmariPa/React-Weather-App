afd9a75641e4dc08e48434a83d16fbe523ecc2e9
Amari Pascoe <elliotpascoe04@gmail.com>
2023-06-01 16:22:09 -0400

    updated background image and search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,92 @@
-import { CircularProgress, Slide, TextField } from "@mui/material";
-import { useEffect, useState } from "react";
-import "./App.css";
+import { CircularProgress, Slide, TextField } from '@mui/material';
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
-  const [cityName, setCityName] = useState("Tokyo");
-  const [inputText, setInputText] = useState("");
-  const [data, setData] = useState({});
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(true);
+	const [cityName, setCityName] = useState('Tokyo');
+	const [inputText, setInputText] = useState('');
+	const [data, setData] = useState({});
+	const [error, setError] = useState(false);
+	const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=33ab7a6a924a45c4c1dfbc566400cc78&units=imperial`
-    )
-      .then((res) => {
-        if (res.status === 200) {
-          error && setError(false);
-          return res.json();
-        } else {
-          throw new Error("Something went wrong");
-        }
-      })
-      .then((data) => {
-        setData(data);
-      })
-      .catch(() => setError(true))
-      .finally(() => setLoading(false));
-  }, [cityName, error]);
+	useEffect(() => {
+		fetch(
+			`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=33ab7a6a924a45c4c1dfbc566400cc78&units=imperial`
+		)
+			.then((res) => {
+				if (res.status === 200) {
+					error && setError(false);
+					return res.json();
+				} else {
+					throw new Error('Something went wrong');
+				}
+			})
+			.then((data) => {
+				setData(data);
+			})
+			.catch(() => setError(true))
+			.finally(() => setLoading(false));
+	}, [cityName, error]);
 
-  const handleSearch = (e) => {
-    if (e.key === "Enter") {
-      setCityName(e.target.value);
-      setInputText("");
-    }
-  };
+	const handleSearch = (e) => {
+		if (e.key === 'Enter') {
+			setCityName(e.target.value);
+			setInputText('');
+		}
+	};
 
-  return (
-    <div className="bg_img">
-      {!loading ? (
-        <>
-          <TextField
-            variant="filled"
-            label="Search location"
-            className="input"
-            error={error}
-            value={inputText}
-            onChange={(e) => setInputText(e.target.value)}
-            onKeyDown={handleSearch}
-          />
-          <h1 className="city">{data.name}</h1>
-          <div className="group">
-            <img
-              src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
-              alt=""
-            />
-            <h1>{data.weather[0].main}</h1>
-          </div>
+	return (
+		<div className='bg_img'>
+			{!loading ? (
+				<>
+					<TextField
+						variant='outlined'
+						disableUnderline
+						label='Search...'
+						className='input'
+						error={error}
+						value={inputText}
+						onChange={(e) => setInputText(e.target.value)}
+						onKeyDown={handleSearch}
+						sx={{
+							border: '2px solid white',
+						}}
+					/>
+					<h1 className='city'>{data.name}</h1>
+					<div className='group'>
+						<img
+							src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+							alt=''
+						/>
+						<h1>{data.weather[0].main}</h1>
+					</div>
 
-          <h1 className="temp">{data.main.temp.toFixed()} °C</h1>
+					<h1 className='temp'>{data.main.temp.toFixed()} °C</h1>
 
-          <Slide direction="right" timeout={800} in={!loading}>
-            <div className="box_container">
-              <div className="box">
-                <p>Humidity</p>
-                <h1>{data.main.humidity.toFixed()}%</h1>
-              </div>
+					<Slide direction='right' timeout={800} in={!loading}>
+						<div className='box_container'>
+							<div className='box'>
+								<p>Humidity</p>
+								<h1>{data.main.humidity.toFixed()}%</h1>
+							</div>
 
-              <div className="box">
-                <p>Wind</p>
-                <h1>{data.wind.speed.toFixed()} mp/h</h1>
-              </div>
+							<div className='box'>
+								<p>Wind</p>
+								<h1>{data.wind.speed.toFixed()} mp/h</h1>
+							</div>
 
-              <div className="box">
-                <p>Feels Like</p>
-                <h1>{data.main.feels_like.toFixed()} °F</h1>
-              </div>
-            </div>
-          </Slide>
-        </>
-      ) : (
-        <CircularProgress />
-      )}
-    </div>
-  );
+							<div className='box'>
+								<p>Feels Like</p>
+								<h1>{data.main.feels_like.toFixed()} °F</h1>
+							</div>
+						</div>
+					</Slide>
+				</>
+			) : (
+				<CircularProgress />
+			)}
+		</div>
+	);
 }
 
 export default App;
